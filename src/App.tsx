487ea9a52b99e3a272328ad354ef461265e61e3c
Mugdhashrivastava <shrivastavamugdha19@gmail.com>
2024-07-30 21:09:28 +0530

    displaying the json data on screen with necessary fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,47 @@
-
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface Customer {
-  id: number;
-  firstName: string;
-  lastName: string;
-  birthDate: string;
-  city: string;
+	id: number;
+	firstName: string;
+	lastName: string;
+	birthDate: string;
+	city: string;
 }
 
 const App: React.FC = () => {
-  const [customers, setCustomers] = useState<Customer[]>([]);
-    useEffect(() => {
-    fetch('https://dummyjson.com/users')
-      .then(response => response.json())
-      .then(data => {
-        const formattedData = data.users.map((user: any) => ({
-          id: user.id,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          birthDate: user.birthDate,
-          city: user.address.city,
-        }));
-        setCustomers(formattedData);
-      })
-      .catch(error => console.error('Error fetching customer data:', error));
-  }, []);
+	const [customers, setCustomers] = useState<Customer[]>([]);
+	useEffect(() => {
+		fetch('https://dummyjson.com/users')
+			.then((response) => response.json())
+			.then((data) => {
+				const formattedData = data.users.map((user: any) => ({
+					id: user.id,
+					firstName: user.firstName,
+					lastName: user.lastName,
+					birthDate: user.birthDate,
+					city: user.address.city,
+				}));
+				setCustomers(formattedData);
+			})
+			.catch((error) =>
+				console.error('Error fetching customer data:', error)
+			);
+	}, []);
 
-  
-  return (
-    <div>
-      <h1>Customer List</h1>
-     
-    </div>
-  );
+	return (
+		<div>
+			<h1>Customer List</h1>
+			<ul>
+				{customers.map((customer) => (
+					<li key={customer.id}>
+						{customer.firstName} {customer.lastName} -{' '}
+						{customer.city} -{' '}
+						{new Date(customer.birthDate).toLocaleDateString()}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default App;
