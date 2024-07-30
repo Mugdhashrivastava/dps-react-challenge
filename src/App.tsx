3f1fd89e7ef5d60d0d775daccd7caacb183b5cdc
Mugdhashrivastava <shrivastavamugdha19@gmail.com>
2024-07-30 21:03:26 +0530

    fetching data from dummy json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
 interface Customer {
   id: number;
@@ -11,7 +11,23 @@ interface Customer {
 
 const App: React.FC = () => {
   const [customers, setCustomers] = useState<Customer[]>([]);
+    useEffect(() => {
+    fetch('https://dummyjson.com/users')
+      .then(response => response.json())
+      .then(data => {
+        const formattedData = data.users.map((user: any) => ({
+          id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          birthDate: user.birthDate,
+          city: user.address.city,
+        }));
+        setCustomers(formattedData);
+      })
+      .catch(error => console.error('Error fetching customer data:', error));
+  }, []);
 
+  
   return (
     <div>
       <h1>Customer List</h1>
